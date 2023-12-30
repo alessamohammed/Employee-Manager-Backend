1a0c03bf1d4f2493f4058a5a35d4dd23357c4d41
Mohammed Alessa <m@alessamohammed.com>
2023-12-31 01:46:20 +0300

    Employee CRUD

---
+++ src/main/java/com/alessamohammed/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.alessamohammed.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
