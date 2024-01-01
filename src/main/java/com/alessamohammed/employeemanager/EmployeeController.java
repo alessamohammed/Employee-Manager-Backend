1b0a32aaaf0ad7c5753f534cfb4aa7ba8a22854f
Mohammed Alessa <m@alessamohammed.com>
2024-01-01 03:38:05 +0300

    Fix delete employee issue

--- src/main/java/com/alessamohammed/employeemanager/EmployeeController.java
+++ src/main/java/com/alessamohammed/employeemanager/EmployeeController.java
@@ -2,6 +2,7 @@ package com.alessamohammed.employeemanager;
 
 import com.alessamohammed.employeemanager.model.Employee;
 import com.alessamohammed.employeemanager.service.EmployeeService;
+import jakarta.transaction.Transactional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Transactional
 @RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
